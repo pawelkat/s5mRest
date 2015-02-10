3d8addf97f880e64ada29f19d8f974bc9a1de9bb
unknown <dhwpaka@DASP303.dzwn.intern>
2015-02-10 16:35:28 +0100

    cards are read-only

--- src/js/S5MLearn.js
+++ src/js/S5MLearn.js
@@ -11,7 +11,10 @@ $(document).ready(function() {
 		renderImages = false,
 		mapModel = new MAPJS.MapModel(MAPJS.KineticMediator.layoutCalculator, ['A brilliant idea...', 'A cunning plan...', 'We\'ll be famous...'], ['hoo har']);
 		container.mapWidget(console,mapModel, isTouch, renderImages);
+		
 		mModel=mapModel;
+		//making map content read-only
+		mModel.setEditingEnabled(false);
 		//initialize buttons
 		var button = $("#grade-noidea").button();
 		button.click(
@@ -103,75 +106,5 @@ app={
 			addclass: "stack-bottomright custom",
 			stack: stack_bottomright
 		});
-	},
-	setGoogleIcon: function() {
-		var opt = {
-			autoOpen: false,
-			modal: true,
-			width: 550,
-			height:650,
-			title: 'Select Google picture'
-		};
-		var theDialog = $("#get-google-icon-dialog").dialog(opt);
-		theDialog.dialog("open");
-		$("#get-google-icon-dialog").dialog("option", "title", "Select Google picture");
-		$("#get-google-icon-dialog").dialog("option", "buttons", { 
-			"cancel": function() { $(this).dialog("close"); },
-			"Select": function() {
-				var selImg=$('#google-panel .ui-selected img');
-				dataUrl = $(selImg).attr('src');
-				imgWidth=$(selImg).width();
-				imgHeight=selImg.height();
-				var scaleX = Math.min(imgWidth, 300) / imgWidth,
-				scaleY = Math.min(imgHeight, 300) / imgHeight,
-				scale = Math.min(scaleX, scaleY);
-				var position = "left";
-				mapModel.setIcon('drag and drop', dataUrl, Math.round(imgWidth * scale), Math.round(imgHeight * scale), position, ideaId);
-				$(this).dialog("close");
-			}
-		});
-		$("#get-google-icon-dialog").dialog("open");
-		var ideaId = mapModel.getSelectedNodeId();
-		var idea = mapModel.findIdeaById(ideaId);
-		keyword=idea.title;
-		imageSearch.setSearchCompleteCallback(this, app.onGoogleSearchComplete, null);
-		imageSearch.execute(keyword);
-		$("#searchGoogleStr").val(keyword);
-		var button = $("#searchGoogleBtn").button();
-		button.click(
-			function(){
-				var str = $("#searchGoogleStr").val();
-				imageSearch.setSearchCompleteCallback(this, app.onGoogleSearchComplete, null);
-				imageSearch.execute(str);
-			}    
-		);
-		
-	},
-
-	onGoogleSearchComplete: function() {
-		// Check that we got results
-		var contentDiv = document.getElementById('google-panel');
-		if (imageSearch.results && imageSearch.results.length > 0) {
-		// Grab our content div, clear it.
-			contentDiv.innerHTML = '';
-
-			var results = imageSearch.results;
-			for (var i = 0; i < results.length; i++) {
-				// For each result image to the screen
-				var result = results[i];
-				var imgContainer = document.createElement('div');
-				var newImg = document.createElement('img');
-				// There is also a result.url property which has the escaped version
-				newImg.src=result.tbUrl;
-				imgContainer.appendChild(newImg);
-				// Put our title + image in the content
-				contentDiv.appendChild(imgContainer);
-			}
-			//clear search 
-			imageSearch.clearResults();
-			$( "#google-panel" ).selectable();
-		}else{
-			contentDiv.innerHTML = '<p>Nothing found</p>';
-		}
 	}
 }		
